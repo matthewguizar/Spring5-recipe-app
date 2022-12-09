b3185853458aef03644a8ac8693cb7d09c77f0c7
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-08 17:35:05 -0800

    spring integration test with JPA data

--- src/test/java/com/springframework/spring5recipeapp/respositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/springframework/spring5recipeapp/respositories/UnitOfMeasureRepositoryIT.java
@@ -1,20 +1,41 @@
 package com.springframework.spring5recipeapp.respositories;
 
+import com.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
+@DataJpaTest
 class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
+    //@DirtiesContext restarts instead of just checking once
     void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
     }
 }
