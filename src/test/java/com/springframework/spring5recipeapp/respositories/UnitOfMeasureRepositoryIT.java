2bc8e2f2211c213a8dfa048539a766d8bce2f50c
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-08 17:29:09 -0800

    intro to spring mock mvc

---
+++ src/test/java/com/springframework/spring5recipeapp/respositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,20 @@
+package com.springframework.spring5recipeapp.respositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+class UnitOfMeasureRepositoryIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+    }
+}
