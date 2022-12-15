c6b7acc216d94caa56787cf2b827061cb88ff349
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-15 15:24:19 -0800

    persisting images to database

--- src/main/java/com/springframework/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/springframework/spring5recipeapp/services/ImageServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 
 @Slf4j
@@ -19,6 +20,7 @@ public class ImageServiceImpl implements ImageService{
     }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
         try{
             Recipe recipe = recipeRepository.findById(recipeId).get();
