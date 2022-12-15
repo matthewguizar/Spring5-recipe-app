650b78ec8f386d1f6f42c195509be611ef6a2f98
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-14 17:32:16 -0800

    implemented delete ingredient

--- src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
+++ src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
@@ -7,4 +7,6 @@ public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 
     IngredientCommand saveIngredientCommand(IngredientCommand savedCommand);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
